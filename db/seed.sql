fcf29d8a1ac6d3b85c883211691733c3d62c9d25
amshepard <antoineshepard@pursuit.org>
2023-08-07 20:46:32 -0400

    Intial commit.

---
+++ db/seed.sql
@@ -0,0 +1,25 @@
+\c tuner
+
+
+INSERT INTO kicks (name, brand, price, release_date,) VALUES
+    ('Air Force One', 'Nike', 08995, '19820301'),
+    ('Jordan 1', 'Nike', 06499, '19850401'),
+    ('Superstar', 'Adidas', 06499, '19700301'),
+    ('Kamikaze', 'Reebok', 12499, '19951101'),
+    ('Aero Jam Mid', 'Converse', 07499, '19930401'),
+    ('Air Max 95 OG', 'Nike', 14999, '19950301'),
+    ('Air Yeezy', 'Nike', 21499, '20090404'),
+    ('Kaws Bapesta', 'A Bathing Ape', 15000, '20060101'),
+    ('DRKSHDW Strobe Hexa', 'Rick Owens', 74500, '20220720'),
+    -- ('Air Force One', 'Nike', 08995, '19820301'),
+    -- ('Jordan 1', 'Nike', 06499, '19850401'),
+    -- ('Superstar', 'Adidas', 06499, '19700301'),
+
+    
+    -- ('Fame', 'David Bowie', 'Young Americans', '4:12', true ),
+    -- ('Once in a Lifetime', 'Talking Heads', 'Remain in Light', '4:19', true ),
+    -- ('The Great Curve', 'Talking Heads', 'Sand in the Vaseline', '5:39', true ),
+    -- ('(Nothing But) Flowers',  'Talking Heads', 'Remain in Light', '6:28', false ),
+    -- ('Books about UFOs', 'Hüsker Dü', 'New Day Rising', '2:49', true ),
+    -- ('Mr. Startup', 'Wolf Parade', 'Thin Mind', '3:31', true ),
+    -- ('We Got the World', 'Icona Pop', 'This is...', '3:17', false );
